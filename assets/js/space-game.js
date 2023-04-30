62e689d3baa34a03ec1774a30f6dce3e2a112a7c
DanyLC91 <daniela.campisi@hotmail.com>
2023-04-30 07:26:54 +0000

    Setting up ship it will take 2 tiles weidth x 1 height

--- assets/js/space-game.js
+++ assets/js/space-game.js
@@ -7,3 +7,17 @@ let board;
 let boardWidth = tileSize * columns; 
 let boardHeight = tileSize * rows;
 let context;
+
+//ship
+let shipWidth = tileSize*2;
+let shipHeight = tileSize;
+let shipX = tileSize * columns/2 - tileSize;
+let shipY = tileSize * rows - tileSize*2;
+let ship = {
+    x : shipX,
+    y : shipY,
+    width : shipWidth,
+    height : shipHeight
+}
+let shipImg;
+let shipVelocityX = tileSize;
